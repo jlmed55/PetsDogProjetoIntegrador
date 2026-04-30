ac1d235385278f34c5d494f4a75107e3b3aeecc4
jpmed123 <programadorjp13@gmail.com>
2026-04-29 21:50:22 -0300

    fixing merge

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import {  Router } from "express";
 
 import clienteRoutes from "./modules/cliente/cliente.routes.js";
 
+
+
 const routes = Router();
 
 routes.get("/health", ( resquest, response) => {
@@ -11,6 +13,9 @@ routes.get("/health", ( resquest, response) => {
 });
 
 
+
 routes.use("/clientes", clienteRoutes)
+
+
 
 export default routes;
