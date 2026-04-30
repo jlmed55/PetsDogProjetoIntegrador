7b6646d0b4da02b9e251e1146c6a793f2115613f
jpmed123 <programadorjp13@gmail.com>
2026-04-29 21:52:59 -0300

    fixing routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,7 @@
 import {  Router } from "express";
-
 import clienteRoutes from "./modules/cliente/cliente.routes.js";
-
-
+import servicoRoutes from "./modules/servico/servico.routes.js";
+import profissionalRoutes from "./modules/profissional/profissional.routes.js";
 
 const routes = Router();
 
@@ -12,10 +11,8 @@ routes.get("/health", ( resquest, response) => {
    })
 });
 
-
-
 routes.use("/clientes", clienteRoutes)
-
-
+routes.use("/servicos", servicoRoutes)
+routes.use("/profissionais", profissionalRoutes)
 
 export default routes;
